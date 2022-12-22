3a518f85ce3629381f8c088846edf8c08412bde6
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-12-22 15:52:11 +0100

    MileStone0

---
+++ PlayerContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPlayers
+{
+    internal class PlayerContext :DbContext
+    {
+  
+
+            public DbSet<Player> Player { get; set; }
+            public DbSet<Team> Team { get; set; }
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("Data Source=localhost;Database=DBPlatersEF;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+            }
+        }
+}
