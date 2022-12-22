b56c4b255fc7e815fb0b6a3949f3e42e44358835
Nikola Krstevski <nikolakrstevskids@gmail.com>
2022-12-22 15:44:46 +0100

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPlayers
+{
+    internal class Player
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string cognome { get; set; }
+        [Required]
+        public double punteggio { get; set; }
+        [Required]
+        public int numero_giocate { get; set; }
+        [Required]
+        public int numero_vinte { get; set; }
+        public int teamid { get; set; }
+        public Team team { get; set; }
+    }
+}
---
+++ Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPlayers
+{
+    internal class Team
+    {
+        [Required]
+        public string nome { get; set; }
+        [Required]
+        public string citta { get; set; }
+        [Required]
+        public string allenatore { get; set; }
+        [Required]
+        public string colori { get; set; }
+        public List<Player> players { get; set; }
+    }
+}
